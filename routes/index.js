c77121feff0b30dd44d424918aa243d44b3acc5c
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-05-12 21:06:11 +0700

    Update Yup validation

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Welcome to Express' });
+// });
+
+// router.get('/send', async (req, res, next) => {
+//   res.send({ message: 'Hello' });
+// });
+
+// router.get('/sendStatus', async (req, res, next) => {
+//   res.sendStatus(404);
+// });
+
+// Thường dùng
+// router.get('/setStatus', async (req, res, next) => {
+//   res.status(201).json({ message: 'Hello' });
+// });
+
+router.get('/sendFile', async (req, res, next) => {
+  res.sendFile('/public/stylesheets/style.css', { root: './' });
+});
+
+// Thường dùng (status = 200)
+router.get('/json', async (req, res, next) => {
+  res.json({ message: 'Hello Redirect' });
+});
+
+router.get('/redirect', async (req, res, next) => {
+  res.redirect('/json');
 });
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const yup = require('yup');
 
 const { generationID, writeFileSync } = require('../utils');
 
@@ -19,50 +20,141 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
   const id = req.params.id;
 
-  const product = products.find((p) => p.id.toString() === id.toString());
-
-  if (!product) {
-    res.status(404).json({
-      code: 4041,
-      message: 'Get detail fail!!',
-    });
-  }
-
-  res.status(200).json({
-    code: 2001,
-    message: 'Get detail success!!',
-    payload: product,
+  const validationSchema = yup.object().shape({
+    id: yup.number().test('Kiem_tra_do_dai_id', 'Sai định dạng rồi mài', val => {
+      return val.length === 13
+    }),
   });
+
+  validationSchema
+    .validate(req.params)
+    .then(() => {
+      console.log('Validation passed');
+
+      const product = products.find((p) => p.id.toString() === id.toString());
+
+      if (!product) {
+        res.status(404).json({
+          code: 4041,
+          message: 'Get detail fail!!',
+        });
+      }
+    
+      res.status(200).json({
+        code: 2001,
+        message: 'Get detail success!!',
+        payload: product,
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: 'Get detail fail!!',
+        payload: err,
+      });
+    });
 });
 
 router.post('/', function(req, res, next) {
-  const { name, price, description, discount, supplierId } = req.body;
+  const { name, price, description, discount, phone, supplierId } = req.body;
 
-  const initID = generationID();
+  const phoneRegExp = /(84|0[3|5|7|8|9])+([0-9]{8})\b/;
 
-  const newProduct = {
-    id: initID,
-    name,
-    price,
-    description,
-    discount,
-    supplierId,
-  };
+  const validationSchema = yup.object().shape({
+    body: yup.object({
+      name: yup.string().max(50).required(),
+      price: yup.number().min(0, 'Giá phải lớn hơn hoặc bằng 0').required(),
+      description: yup.string(),
+      phone: yup.string().matches(phoneRegExp, 'SỐ ĐIỆN THOẠI BỊ SAI').required('Số dt không được bỏ trống'),
+      supplierId: yup.number().test('Kiem_tra_do_dai_id', 'Sai định dạng rồi mài', val => {
+        if (!val) return true;
+  
+        return val.length === 13
+      }),
+      // isHasWife: yup.bool(),
+      // wifeName: yup.string().test({
+      //   message: 'Tên vợ không được để trống',
+      //   test(value) {
+      //       if (this.parent.isHasWife && !value) return false;
+    
+      //       return true;
+      //     },
+      // })
+    })
+  });  
 
-  products.push(newProduct);
+  validationSchema
+  .validate({ body: req.body}, { abortEarly: false })
+  .then(() => {
+    console.log('Validation passed');
 
-  const newP = products.find((p) => p.id.toString() === initID.toString());
+    const initID = generationID();
 
-  writeFileSync('./data/products.json', products);
+    const newProduct = {
+      id: initID,
+      name,
+      price,
+      description,
+      discount,
+      phone,
+      // supplierId: supplierId && supplierId,
+      // ...(supplierId && { supplierId }),
+    };
 
-  res.status(201).json({
-    code: 2011,
-    message: 'Created success!!',
-    payload: newP,
+    if (supplierId) {
+      newProduct.supplierId = supplierId;
+    }
+
+    products.push(newProduct);
+
+    const newP = products.find((p) => p.id.toString() === initID.toString());
+  
+    writeFileSync('./data/products.json', products);
+  
+    res.status(201).json({
+      code: 2011,
+      message: 'Created success!!',
+      payload: newP,
+    });
+  })
+  .catch((err) => {
+    return res.status(400).json({
+      type: err.name,
+      errors: err.errors,
+      provider: 'yup'
+    });
   });
+
 });
 
 router.patch('/:id', function(req, res, next) {
+  // const getProductsSchema = yup.object({
+  //   query: yup.object({
+  //     category: yup.string().test('Validate ObjectID', '${path} is not valid ObjectID', (value) => {
+  //       if (!value) return true;
+  //       return ObjectId.isValid(value);
+  //     }),
+  //     sup: yup.string().test('Validate ObjectID', '${path} is not valid ObjectID', (value) => {
+  //       if (!value) return true;
+  //       return ObjectId.isValid(value);
+  //     }),
+  //     productName: yup.string(),
+  //     stockStart: yup.number().min(0),
+  //     stockEnd: yup.number(),
+  //     priceStart: yup.number().min(0),
+  //     priceEnd: yup.number(),
+  //     discountStart: yup.number().min(0),
+  //     discountEnd: yup.number().max(50),
+  //     skip: yup.number(),
+  //     limit: yup.number(),
+  //   }),
+  // });
+
+  // schema.validate({
+  //   body: req.body,
+  //   query: req.query,
+  //   params: req.params,
+  // })
+
   const { id } = req.params;
 
   const checkProductExits = products.find((p) => p.id.toString() === id.toString());
@@ -143,21 +235,5 @@ router.delete('/:id', function(req, res, next) {
     message: 'Delete success!!',
   });
 });
-
-// router.post('/res', function(req, res, next) {
-//   res.send('this is Post method');
-// });
-
-// router.put('/res', function(req, res, next) {
-//   res.send('this is put method');
-// });
-
-// router.delete('/res', function(req, res, next) {
-//   res.send('this is delete method');
-// });
-
-// router.patch('/res', function(req, res, next) {
-//   res.send('this is patch method');
-// });
 
 module.exports = router;
