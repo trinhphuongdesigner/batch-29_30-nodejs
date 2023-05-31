7e8f1589ce2815cc0f78820f6ee14341bbc17674
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-05-31 21:04:50 +0700

    Update question

--- routes/questions/controller.js
+++ routes/questions/controller.js
@@ -1,4 +1,10 @@
-const { Product, Category, Supplier } = require('../../models');
+const {
+  Product,
+  Category,
+  Supplier,
+  Customer,
+  Order,
+} = require('../../models');
 
 module.exports = {
   question1: async (req, res, next) => {
@@ -161,7 +167,9 @@ module.exports = {
 
       console.log('««««« conditionFind »»»»»', conditionFind);
 
-      let results = await Product.find(conditionFind).lean(); // convert data to object
+      let results = await Product.find(conditionFind)
+        .select('-categoryId -supplierId -description')
+        .lean(); // convert data to object
 
       // const newResults = results.map((item) => {
       //   const dis = item.price * (100 - item.discount) / 100;
@@ -228,10 +236,10 @@ module.exports = {
       // ]);
       // aggregate([])
 
-      let results = await Product
-        .aggregate()
-        .match({ $expr: { $lte: [d, parseFloat(40000)] } })
-      
+      let results = await Product.aggregate().match({
+        $expr: { $lte: [d, parseFloat(40000)] },
+      });
+
       let total = await Product.countDocuments();
 
       return res.send({
@@ -241,6 +249,339 @@ module.exports = {
         payload: results,
       });
     } catch (err) {
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question3c: async (req, res, next) => {
+    try {
+      const s = { $subtract: [100, '$discount'] }; // (100 - 10) s => 90
+      const m = { $multiply: ['$price', s] }; // price * 90
+      const d = { $divide: [m, 100] }; // price * 90 / 100
+
+      // let results = await Product.aggregate([
+      //   { $addFields: { disPrice: d } },
+      //   {
+      //     $match: { $expr: { $lte: ['$disPrice', parseFloat(40000)] } },
+      //   },
+      //   {
+      //     $project: {
+      //       categoryId: 0,
+      //       supplierId: 0,
+      //       description: 0,
+      //     },
+      //   },
+      // ]);
+
+      let results = await Product.aggregate()
+        .addFields({ disPrice: d })
+        .match({ $expr: { $lte: ['$disPrice', parseFloat(40000)] } })
+        .project({
+          categoryId: 0,
+          supplierId: 0,
+          description: 0,
+        });
+
+      let total = await Product.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question4: async (req, res, next) => {
+    try {
+      const { address } = req.query;
+
+      const conditionFind = {
+        address: { $regex: new RegExp(`${address}`), $options: 'i' },
+      };
+      // const conditionFind = { address: new RegExp(`${address}`) };
+      // const conditionFind = { address: {$eq: address } };
+
+      console.log('««««« conditionFind »»»»»', conditionFind);
+
+      let results = await Customer.find(conditionFind);
+
+      let total = await Customer.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question4a: async (req, res, next) => {
+    try {
+      const { address } = req.query;
+
+      // const conditionFind = { address: { $regex: new RegExp(`${address}`), $options: 'i' } };
+      // const conditionFind = { address: new RegExp(`${address}`) };
+      // const conditionFind = { address: {$eq: address } };
+
+      let results = await Customer.aggregate().match({
+        address: { $regex: new RegExp(`${address}`), $options: 'i' },
+      });
+
+      let total = await Customer.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question5: async (req, res, next) => {
+    try {
+      const { year } = req.query;
+
+      const conditionFind = {
+        $expr: {
+          $eq: [{ $year: '$birthday' }, year],
+        },
+      };
+
+      console.log('««««« conditionFind »»»»»', conditionFind);
+
+      let results = await Customer.find(conditionFind);
+
+      let total = await Customer.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question6: async (req, res, next) => {
+    try {
+      const { date } = req.query;
+      let today;
+
+      if (!date) {
+        today = new Date();
+      } else {
+        today = new Date(date);
+      }
+
+      const conditionFind = {
+        $expr: {
+          $and: [
+            {
+              $eq: [{ $dayOfMonth: '$birthday' }, { $dayOfMonth: today }],
+            },
+            { $eq: [{ $month: '$birthday' }, { $month: today }] },
+          ],
+        },
+      };
+
+      // const eqDay = {
+      //   $eq: [{ $dayOfMonth: '$birthday' }, { $dayOfMonth: today }],
+      // };
+      // const eqMonth = { $eq: [{ $month: '$birthday' }, { $month: today }] };
+
+      // const conditionFind = {
+      //   $expr: {
+      //     $and: [eqDay, eqMonth],
+      //   },
+      // };
+
+      console.log('««««« conditionFind »»»»»', conditionFind);
+
+      let results = await Customer.find(conditionFind);
+
+      let total = await Customer.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question7: async (req, res, next) => {
+    try {
+      const { status } = req.query;
+
+      let results = await Order.find({ status })
+        .populate({ path: 'customer', select: 'firstName lastName' })
+        .populate('employee')
+        .populate({
+          path: 'orderDetails.product',
+          select: { name: 1, stock: 1 },
+        })
+        .lean();
+
+      let total = await Order.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question7a: async (req, res, next) => {
+    try {
+      const { status } = req.query;
+
+      let results = await Order.aggregate()
+        .match({ status })
+        .lookup({
+          from: 'customers',
+          localField: 'customerId',
+          foreignField: '_id',
+          as: 'customer',
+        })
+        .unwind('customer')
+        .lookup({
+          from: 'employees',
+          localField: 'employeeId',
+          foreignField: '_id',
+          as: 'employee',
+        })
+        .unwind('employee')
+        .project({
+          customerId: 0,
+          employeeId: 0,
+        });
+      // .lookup({
+      //   from: 'products',
+      //   localField: 'orderDetails.productId',
+      //   foreignField: '_id',
+      //   as: 'orderDetails.product',
+      // })
+      // .unwind('product')
+      // .populate({ path: 'customer', select: 'firstName lastName' })
+      // .populate('employee')
+      // .populate({
+      //   path: 'orderDetails.product',
+      //   select: { name: 1 , stock: 1},
+      // })
+      // .select('-customerId -employeeId -orderDetails.productId')
+      // .lean();
+
+      let total = await Order.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question8a: async (req, res, next) => {
+    try {
+      let { status, date } = req.query;
+      const findDate = date ? new Date(date) : new Date();
+
+      const conditionFind = {
+        $expr: {
+          $and: [
+            // { $eq: ['$status', status] },
+            { status },
+            { $eq: [{ $dayOfMonth: '$shippedDate' }, { $dayOfMonth: findDate }] },
+            { $eq: [{ $month: '$shippedDate' }, { $month: findDate }] },
+            { $eq: [{ $year: '$shippedDate' }, { $year: findDate }] },
+            
+          ],
+        },
+      };
+
+      console.log('««««« conditionFind »»»»»', conditionFind);
+
+      let results = await Order.find(conditionFind)
+        .lean();
+
+      let total = await Order.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question8b: async (req, res, next) => {
+    try {
+      let { status, fromDate, toDate } = req.query;
+
+      fromDate = new Date(fromDate);
+      fromDate.setHours(0, 0, 0, 0);
+
+      const tmpToDate = new Date(toDate);
+      tmpToDate.setHours(0, 0, 0, 0);
+      toDate = new Date(tmpToDate.setDate(tmpToDate.getDate() + 1));
+  
+      const compareStatus = { $eq: ['$status', status] };
+      const compareFromDate = { $gte: ['$shippedDate', fromDate] };
+      const compareToDate = { $lt: ['$shippedDate', toDate] };
+  
+      const conditionFind = {
+        $expr: { $and: [compareStatus, compareFromDate, compareToDate] },
+      };
+
+      let results = await Order.find(conditionFind)
+        .populate('orderDetails.product')
+        .populate('customer')
+        .populate('employee')
+        .lean();
+
+      let total = await Order.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      console.log('««««« err »»»»»', err);
       return res.status(500).json({ code: 500, error: err });
     }
   },
--- routes/questions/router.js
+++ routes/questions/router.js
@@ -15,6 +15,15 @@ const {
   question3,
   question3a,
   question3b,
+  question3c,
+  question4,
+  question4a,
+  question5,
+  question6,
+  question7,
+  question7a,
+  question8a,
+  question8b,
 } = require('./controller');
 
 router.get('/1', question1);
@@ -25,5 +34,14 @@ router.get('/2b', question2b);
 router.get('/3', question3);
 router.get('/3a', validateSchema(q3a), question3a);
 router.get('/3b', question3b);
+router.get('/3c', question3c);
+router.get('/4', question4);
+router.get('/4a', question4a);
+router.get('/5', question5);
+router.get('/6', question6);
+router.get('/7', question7);
+router.get('/7a', question7a);
+router.get('/8a', question8a);
+router.get('/8b', question8b);
 
 module.exports = router;
